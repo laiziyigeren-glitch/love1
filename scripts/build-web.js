8a134d7fea207083fa50b26e9d19493d7371a621
wangbiao2005 <430886453@qq.com>
2026-05-03 22:52:28 +0800

    Fix clean love page URL on Cloudflare Pages

--- scripts/build-web.js
+++ scripts/build-web.js
@@ -27,13 +27,9 @@ fs.rmSync(outDir, { recursive: true, force: true });
 fs.mkdirSync(outDir, { recursive: true });
 
 copyFile(path.join(root, 'you-and-me.html'), path.join(outDir, 'you-and-me.html'));
+copyFile(path.join(root, 'you-and-me.html'), path.join(outDir, 'you-and-me', 'index.html'));
 copyFile(path.join(root, 'index.html'), path.join(outDir, 'index.html'));
 copyFile(path.join(root, 'config.example.js'), path.join(outDir, 'config.js'));
 copyDir(path.join(root, '爱心代码合集'), path.join(outDir, 'heart-garden'));
-
-fs.writeFileSync(
-  path.join(outDir, '_redirects'),
-  ['/you-and-me /you-and-me.html 200', '/you-and-me/ /you-and-me.html 200', ''].join('\n'),
-);
 
 console.log(`Built front-end static files into ${outDir}`);
